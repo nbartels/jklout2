c24c76f07d3f2467bae9fdbd2bbb301d5df4a3d9
nbartels <n.bartels@phpmonkeys.de>
2013-03-12 23:22:35 +0100

    unittest enhanced

--- src/test/java/jKlout2/TestKlout.java
+++ src/test/java/jKlout2/TestKlout.java
@@ -4,6 +4,7 @@
  */
 package jKlout2;
 
+import jKlout2.types.User;
 import java.io.IOException;
 import java.io.InputStream;
 import org.apache.commons.io.IOUtils;
@@ -15,12 +16,19 @@ public class TestKlout {
 
     @Test
     public void testShowUser() throws IOException, KloutException {
+        // load json file into string
         InputStream is = getClass().getClassLoader().getResourceAsStream("jKlout2/showUser.json");
         String myJson = IOUtils.toString(is, "UTF-8");
+        
+        // HttpConnector mock
         HttpConnector connector = Mockito.mock(HttpConnector.class);
         Mockito.when(connector.getContent()).thenReturn(myJson);
+        
         KloutFactory factory = new KloutFactory();
         Klout kloutClient = factory.setKloutAPIkey("example").setHttpConnector(connector).build();
-        Assert.assertNotNull(kloutClient.getUser("anyid"));
+        User testUser = kloutClient.getUser("anyid");
+        
+        // begin with assertions
+        Assert.assertEquals("jtimberlake", testUser.getNick());
     }
 }
