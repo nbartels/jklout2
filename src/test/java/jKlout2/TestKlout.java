c87f2e37c9f28706b46fd85d384d966bfd82d93b
nbartels <n.bartels@phpmonkeys.de>
2013-03-13 21:09:13 +0100

    more unittests (topics ans score)

--- src/test/java/jKlout2/TestKlout.java
+++ src/test/java/jKlout2/TestKlout.java
@@ -4,9 +4,12 @@
  */
 package jKlout2;
 
+import jKlout2.types.Score;
+import jKlout2.types.Topic;
 import jKlout2.types.User;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -43,12 +46,42 @@ public class TestKlout {
     
     @Test
     public void testUserScore() throws IOException, KloutException {
-        Assert.assertTrue("Not yet implemented", false); 
+        // load json file into string
+        InputStream is = getClass().getClassLoader().getResourceAsStream("jKlout2/score.json");
+        String myJson = IOUtils.toString(is, "UTF-8");
+        
+        // HttpConnector mock
+        HttpConnector connector = Mockito.mock(HttpConnector.class);
+        Mockito.when(connector.getContent()).thenReturn(myJson);
+        
+        KloutFactory factory = new KloutFactory();
+        Klout kloutClient = factory.setKloutAPIkey("example").setHttpConnector(connector).build();
+        Score testScore = kloutClient.getUserScore("anyid");
+        
+        // begin with assertions
+        Assert.assertEquals(92.8064108888354, testScore.getScore(), DELTA);
+        Assert.assertNotNull(testScore.getScoreDelta());
+        Assert.assertEquals(0.11171073367142981D, testScore.getScoreDelta().getDayChange(), DELTA);
+        Assert.assertEquals(0.19782863190633293D, testScore.getScoreDelta().getMonthChange(), DELTA);
+        Assert.assertEquals(0.07298920976049317D, testScore.getScoreDelta().getWeekChange(), DELTA);
     }
     
     @Test
     public void testTopics() throws IOException, KloutException {
-        Assert.assertTrue("Not yet implemented", false); 
+        // load json file into string
+        InputStream is = getClass().getClassLoader().getResourceAsStream("jKlout2/topics.json");
+        String myJson = IOUtils.toString(is, "UTF-8");
+        
+        // HttpConnector mock
+        HttpConnector connector = Mockito.mock(HttpConnector.class);
+        Mockito.when(connector.getContent()).thenReturn(myJson);
+        
+        KloutFactory factory = new KloutFactory();
+        Klout kloutClient = factory.setKloutAPIkey("example").setHttpConnector(connector).build();
+        List<Topic> topicList = kloutClient.getTopics("anyid");
+        
+        // begin with assertions
+        Assert.assertEquals(5, topicList.size());
     }
     
     @Test
