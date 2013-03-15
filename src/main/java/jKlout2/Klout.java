428540e7aeb0122aed75dcf445d96421525c9211
nbartels <n.bartels@phpmonkeys.de>
2013-03-15 15:03:29 +0100

    first comments added to klout interface

--- src/main/java/jKlout2/Klout.java
+++ src/main/java/jKlout2/Klout.java
@@ -9,20 +9,67 @@ import jKlout2.model.User;
 import java.util.List;
 
 public interface Klout {
-    
+
+    /**
+     * mapping from twitter id to the klout id
+     *
+     * @param twitterId
+     * @return a {@see Identity} object to the given twitter id
+     * @throws KloutException
+     */
     public Identity getIdentityFromTwitterID(String twitterId) throws KloutException;
-    
+
+    /**
+     * mapping from google plus id to the klout id
+     *
+     * @param googlePlusId
+     * @return a {@see Identity} object to the given google plus id
+     * @throws KloutException
+     */
     public Identity getIdentityFromGooglePlusID(String googlePlusId) throws KloutException;
-    
+
+    /**
+     * mapping from twitter screen name to the klout id
+     *
+     * @param screenName
+     * @return a {@see Identity} object to the given twitter screen name
+     * @throws KloutException
+     */
     public Identity getIdentityFromTwitterScreenName(String screenName) throws KloutException;
-    
+
+    /**
+     * mapping from klout id to the network id, the target network needs to be
+     * provided
+     *
+     * @param kloutID
+     * @param targetNetwork
+     * @return a {@see Identity} object to the given klout id
+     * @throws KloutException
+     */
     public Identity getIdentityFromKloutID(String kloutID, KloutNetwork targetNetwork) throws KloutException;
 
     public Score getUserScore(String userId) throws KloutException;
 
     public User getUser(String userId) throws KloutException;
 
+    /**
+     * requests the list of topics that the user with the klout id talks about
+     * in his posts (Google or Twitter)
+     *
+     * @param userId
+     * @return a list of topics to the given klout id
+     * @throws KloutException
+     */
     public List<Topic> getTopics(String userId) throws KloutException;
 
+    /**
+     * requests the influencers and influencees of the given klout id if you
+     * don't provide a partner api key you will only get the first 5 influencers
+     * and 5 influencees
+     *
+     * @param userId
+     * @return the {@see Influence} object to the given klout id
+     * @throws KloutException
+     */
     public Influence getInfluence(String userId) throws KloutException;
 }
